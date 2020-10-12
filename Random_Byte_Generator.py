4e3d8d9669d8bee856d59e5616b0eeb1c76eb5c1
g1gantino <37074696+g1gantino@users.noreply.github.com>
2020-10-12 16:44:04 +0200

    Fixed stuff..

--- Random_Byte_Generator.py
+++ Random_Byte_Generator.py
@@ -1,4 +1,5 @@
 import random
+import time
 # /////////////////////////////////
 #    This is my first py program, it's terrible, has spaghetti code, I only posted it here so I can see how  I improve... 
 # /////////////////////////////////
@@ -75,62 +76,6 @@ nine_second_combination = ("__asm _emit " + zero + ("x") + nine_random_number + 
 ten_first_combination = ("__asm _emit " + zero + ("x") + ten_r_alphabet + ten_random_number + " " + "/")
 ten_second_combination = ("__asm _emit " + zero + ("x") + ten_random_number + ten_r_alphabet + " " + "/")
 # __________________________
-times = input("EfefefaadfsdfasDFSdf")
-if times: 1
-print (first_combination)
-if times: 2
-print (two_first_combination)
-print (three_second_combination)
-if times: 3
-print (three_first_combination)
-print (four_second_combination)
-print (first_combination)
-if times: 4
-print (four_first_combination)
-print (five_second_combination)
-print (nine_first_combination)
-print (ten_second_combination)
-if times: 5
-print (five_first_combination)
-print (six_second_combination)
-print (two_first_combination)
-print (three_second_combination)
-print (ten_first_combination)
-if times: 6
-print (six_first_combination)
-print (nine_second_combination)
-print (seven_first_combination)
-print (ten_second_combination)
-print (eight_first_combination)
-print (second_combination)
-if times: 7
-print (seven_first_combination)
-print (second_combination)
-print (six_first_combination)
-print (two_second_combination)
-print (ten_first_combination)
-print (first_combination)
-print (three_second_combination)
-if times: 8
-print (eight_first_combination)
-print (two_second_combination)
-print (nine_first_combination)
-print (three_first_combination)
-print (ten_second_combination)
-print (first_combination)
-print (four_second_combination)
-print (five_first_combination)
-if times: 9
-print (eight_first_combination)
-print (two_second_combination)
-print (nine_first_combination)
-print (four_first_combination)
-print (ten_second_combination)
-print (first_combination)
-print (seven_second_combination)
-print (three_first_combination)
-print (six_second_combination)
-if times: 10
 print (first_combination)
 print (two_second_combination)
 print (three_first_combination)
@@ -140,8 +85,171 @@ print (six_second_combination)
 print (seven_first_combination)
 print (eight_second_combination)
 print (nine_first_combination)
-print (ten_second_combination)
-
+del first_combination
+del two_second_combination
+del three_first_combination
+del four_second_combination
+del five_first_combination
+del six_second_combination
+del seven_first_combination
+del eight_second_combination
+del nine_first_combination
+#first
+first_combination = ("__asm _emit " + zero + ("x") + r_alphabet + random_number + " " + "/")
+second_combination = ("__asm _emit " + zero + ("x") + random_number + r_alphabet + " " + "/")
+#two
+two_first_combination = ("__asm _emit " + zero + ("x") + two_r_alphabet + two_random_number + " " + "/")
+two_second_combination = ("__asm _emit " + zero + ("x") + two_random_number + two_r_alphabet + " " + "/")
+#three
+three_first_combination = ("__asm _emit " + zero + ("x") + three_r_alphabet + three_random_number + " " + "/")
+three_second_combination = ("__asm _emit " + zero + ("x") + three_random_number + three_r_alphabet + " " + "/")
+#four
+four_first_combination = ("__asm _emit " + zero + ("x") + four_r_alphabet + four_random_number + " " + "/")
+four_second_combination = ("__asm _emit " + zero + ("x") + four_random_number + four_r_alphabet + " " + "/")
+#five
+five_first_combination = ("__asm _emit " + zero + ("x") + five_r_alphabet + five_random_number + " " + "/")
+five_second_combination = ("__asm _emit " + zero + ("x") + five_random_number + five_r_alphabet + " " + "/")
+#six
+six_first_combination = ("__asm _emit " + zero + ("x") + six_r_alphabet + six_random_number + " " + "/")
+six_second_combination = ("__asm _emit " + zero + ("x") + six_random_number + six_r_alphabet + " " + "/")
+#seven
+seven_first_combination = ("__asm _emit " + zero + ("x") + seven_r_alphabet + seven_random_number + " " + "/")
+seven_second_combination = ("__asm _emit " + zero + ("x") + seven_random_number + seven_r_alphabet + " " + "/")
+#eight
+eight_first_combination = ("__asm _emit " + zero + ("x") + eight_r_alphabet + eight_random_number + " " + "/")
+eight_second_combination = ("__asm _emit " + zero + ("x") + eight_random_number + eight_r_alphabet + " " + "/")
+#nine
+nine_first_combination = ("__asm _emit " + zero + ("x") + nine_r_alphabet + nine_random_number + " " + "/")
+nine_second_combination = ("__asm _emit " + zero + ("x") + nine_random_number + nine_r_alphabet + " " + "/")
+#ten
+ten_first_combination = ("__asm _emit " + zero + ("x") + ten_r_alphabet + ten_random_number + " " + "/")
+ten_second_combination = ("__asm _emit " + zero + ("x") + ten_random_number + ten_r_alphabet + " " + "/")
+# __________________________________
+print (first_combination)
+print (two_second_combination)
+print (three_first_combination)
+print (four_second_combination)
+print (five_first_combination)
+print (six_second_combination)
+print (seven_first_combination)
+print (eight_second_combination)
+print (nine_first_combination)
+print (first_combination)
+print (two_second_combination)
+print (three_first_combination)
+print (four_second_combination)
+print (five_first_combination)
+print (six_second_combination)
+print (seven_first_combination)
+print (eight_second_combination)
+print (nine_first_combination)
+del first_combination
+del two_second_combination
+del three_first_combination
+del four_second_combination
+del five_first_combination
+del six_second_combination
+del seven_first_combination
+del eight_second_combination
+del nine_first_combination
+#first
+first_combination = ("__asm _emit " + zero + ("x") + r_alphabet + random_number + " " + "/")
+second_combination = ("__asm _emit " + zero + ("x") + random_number + r_alphabet + " " + "/")
+#two
+two_first_combination = ("__asm _emit " + zero + ("x") + two_r_alphabet + two_random_number + " " + "/")
+two_second_combination = ("__asm _emit " + zero + ("x") + two_random_number + two_r_alphabet + " " + "/")
+#three
+three_first_combination = ("__asm _emit " + zero + ("x") + three_r_alphabet + three_random_number + " " + "/")
+three_second_combination = ("__asm _emit " + zero + ("x") + three_random_number + three_r_alphabet + " " + "/")
+#four
+four_first_combination = ("__asm _emit " + zero + ("x") + four_r_alphabet + four_random_number + " " + "/")
+four_second_combination = ("__asm _emit " + zero + ("x") + four_random_number + four_r_alphabet + " " + "/")
+#five
+five_first_combination = ("__asm _emit " + zero + ("x") + five_r_alphabet + five_random_number + " " + "/")
+five_second_combination = ("__asm _emit " + zero + ("x") + five_random_number + five_r_alphabet + " " + "/")
+#six
+six_first_combination = ("__asm _emit " + zero + ("x") + six_r_alphabet + six_random_number + " " + "/")
+six_second_combination = ("__asm _emit " + zero + ("x") + six_random_number + six_r_alphabet + " " + "/")
+#seven
+seven_first_combination = ("__asm _emit " + zero + ("x") + seven_r_alphabet + seven_random_number + " " + "/")
+seven_second_combination = ("__asm _emit " + zero + ("x") + seven_random_number + seven_r_alphabet + " " + "/")
+#eight
+eight_first_combination = ("__asm _emit " + zero + ("x") + eight_r_alphabet + eight_random_number + " " + "/")
+eight_second_combination = ("__asm _emit " + zero + ("x") + eight_random_number + eight_r_alphabet + " " + "/")
+#nine
+nine_first_combination = ("__asm _emit " + zero + ("x") + nine_r_alphabet + nine_random_number + " " + "/")
+nine_second_combination = ("__asm _emit " + zero + ("x") + nine_random_number + nine_r_alphabet + " " + "/")
+#ten
+ten_first_combination = ("__asm _emit " + zero + ("x") + ten_r_alphabet + ten_random_number + " " + "/")
+ten_second_combination = ("__asm _emit " + zero + ("x") + ten_random_number + ten_r_alphabet + " " + "/")
+# __________________________________
+print (first_combination)
+print (two_second_combination)
+print (three_first_combination)
+print (four_second_combination)
+print (five_first_combination)
+print (six_second_combination)
+print (seven_first_combination)
+print (eight_second_combination)
+print (nine_first_combination)
+print (first_combination)
+print (two_second_combination)
+print (three_first_combination)
+print (four_second_combination)
+print (five_first_combination)
+print (six_second_combination)
+print (seven_first_combination)
+print (eight_second_combination)
+print (nine_first_combination)
+del first_combination
+del two_second_combination
+del three_first_combination
+del four_second_combination
+del five_first_combination
+del six_second_combination
+del seven_first_combination
+del eight_second_combination
+del nine_first_combination
+#first
+first_combination = ("__asm _emit " + zero + ("x") + r_alphabet + random_number + " " + "/")
+second_combination = ("__asm _emit " + zero + ("x") + random_number + r_alphabet + " " + "/")
+#two
+two_first_combination = ("__asm _emit " + zero + ("x") + two_r_alphabet + two_random_number + " " + "/")
+two_second_combination = ("__asm _emit " + zero + ("x") + two_random_number + two_r_alphabet + " " + "/")
+#three
+three_first_combination = ("__asm _emit " + zero + ("x") + three_r_alphabet + three_random_number + " " + "/")
+three_second_combination = ("__asm _emit " + zero + ("x") + three_random_number + three_r_alphabet + " " + "/")
+#four
+four_first_combination = ("__asm _emit " + zero + ("x") + four_r_alphabet + four_random_number + " " + "/")
+four_second_combination = ("__asm _emit " + zero + ("x") + four_random_number + four_r_alphabet + " " + "/")
+#five
+five_first_combination = ("__asm _emit " + zero + ("x") + five_r_alphabet + five_random_number + " " + "/")
+five_second_combination = ("__asm _emit " + zero + ("x") + five_random_number + five_r_alphabet + " " + "/")
+#six
+six_first_combination = ("__asm _emit " + zero + ("x") + six_r_alphabet + six_random_number + " " + "/")
+six_second_combination = ("__asm _emit " + zero + ("x") + six_random_number + six_r_alphabet + " " + "/")
+#seven
+seven_first_combination = ("__asm _emit " + zero + ("x") + seven_r_alphabet + seven_random_number + " " + "/")
+seven_second_combination = ("__asm _emit " + zero + ("x") + seven_random_number + seven_r_alphabet + " " + "/")
+#eight
+eight_first_combination = ("__asm _emit " + zero + ("x") + eight_r_alphabet + eight_random_number + " " + "/")
+eight_second_combination = ("__asm _emit " + zero + ("x") + eight_random_number + eight_r_alphabet + " " + "/")
+#nine
+nine_first_combination = ("__asm _emit " + zero + ("x") + nine_r_alphabet + nine_random_number + " " + "/")
+nine_second_combination = ("__asm _emit " + zero + ("x") + nine_random_number + nine_r_alphabet + " " + "/")
+#ten
+ten_first_combination = ("__asm _emit " + zero + ("x") + ten_r_alphabet + ten_random_number + " " + "/")
+ten_second_combination = ("__asm _emit " + zero + ("x") + ten_random_number + ten_r_alphabet + " " + "/")
+# __________________________________
+print (first_combination)
+print (two_second_combination)
+print (three_first_combination)
+print (four_second_combination)
+print (five_first_combination)
+print (six_second_combination)
+print (seven_first_combination)
+print (eight_second_combination)
+print (nine_first_combination)
 
 
 

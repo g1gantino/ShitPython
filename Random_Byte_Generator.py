3ee415d6b0bbc66760a194d1fd27e2603b5914cc
g1gantino <37074696+g1gantino@users.noreply.github.com>
2020-10-13 13:28:05 +0200

    Update Random_Byte_Generator.py

--- Random_Byte_Generator.py
+++ Random_Byte_Generator.py
@@ -1,104 +1,76 @@
-
 # ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 #    This is my first py program, it's terrible, has spaghetti code, I only posted it here so I can see how  I improve... 
 # ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
 import random
 import time
 # __________________________________________
 base = "__asm _emit ";
 zero_to_nine = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
 alphabet = ['a', 'B', 'c', 'd', 'E', 'f', 'G', 'H', 'i', 'J', 'K', 'l', 'M', 'N', 'o', 'P', 'q', 'r', 'S', 't', 'U', 'v', 'W', 'x', 'Y', 'z']
-#__________________fix this________________________|
-'''
-def setrandomnumbers():
-    global random_number, two_random_number, three_random_number, four_random_number, five_random_number, six_random_number, seven_random_number, eight_random_number, nine_random_number, ten_random_number
-
-
-def setrandomalphabet():
-    global random_alphabet, two_random_alphabet, three_random_alphabet, four_random_alphabet, five_random_alphabet, six_random_alphabet, seven_random_alphabet, eight_random_alphabet, nine_random_alphabet, ten_random_alphabet
-
-def setcombinations():
-    global first_combination, two_first_combination, three_first_combination, four_first_combination, five_first_combination, six_first_combination, seven_first_combination, eight_first_combination, nine_first_combination, ten_first_combination
-
-
-def setallvariables():
-    global random_number, two_random_number, three_random_number, four_random_number, five_random_number, six_random_number, seven_random_number, eight_random_number, nine_random_number, ten_random_number
-    global random_alphabet, two_random_alphabet, three_random_alphabet, four_random_alphabet, five_random_alphabet, six_random_alphabet, seven_random_alphabet, eight_random_alphabet, nine_random_alphabet, ten_random_alphabet
-    global first_combination, two_first_combination, three_first_combination, four_first_combination, five_first_combination, six_first_combination, seven_first_combination, eight_first_combination, nine_first_combination, ten_first_combination
-'''
-
 #_________________________________________________________|
 def GetRandomNumbers():
-    #setrandomnumbers()
-    global random_number, two_random_number, three_random_number, four_random_number, five_random_number, six_random_number, seven_random_number, eight_random_number, nine_random_number, ten_random_number
-    random_number = random.choice(zero_to_nine)
-    two_random_number = random.choice(zero_to_nine)
-    three_random_number = random.choice(zero_to_nine)
-    four_random_number = random.choice(zero_to_nine)
-    five_random_number = random.choice(zero_to_nine)
-    six_random_number = random.choice(zero_to_nine)
-    seven_random_number = random.choice(zero_to_nine)
-    eight_random_number = random.choice(zero_to_nine)
-    nine_random_number = random.choice(zero_to_nine)
-    ten_random_number = random.choice(zero_to_nine)
-    #___________________|
+    global random_number1, random_number2, random_number3, random_number4, random_number5, random_number6, random_number7, random_number8, random_number9
+    random_number1 = random.choice(zero_to_nine)
+    random_number2 = random.choice(zero_to_nine)
+    random_number3 = random.choice(zero_to_nine)
+    random_number4 = random.choice(zero_to_nine)
+    random_number5 = random.choice(zero_to_nine)
+    random_number6 = random.choice(zero_to_nine)
+    random_number7 = random.choice(zero_to_nine)
+    random_number8 = random.choice(zero_to_nine)
+    random_number9 = random.choice(zero_to_nine)
+    #___________________
 def GetRandomLetters():
-        #setrandomalphabet()
-        global random_alphabet, two_random_alphabet, three_random_alphabet, four_random_alphabet, five_random_alphabet, six_random_alphabet, seven_random_alphabet, eight_random_alphabet, nine_random_alphabet, ten_random_alphabet
-        random_alphabet = random.choice(alphabet)
-        two_random_alphabet = random.choice(alphabet)
-        three_random_alphabet = random.choice(alphabet)
-        four_random_alphabet = random.choice(alphabet)
-        five_random_alphabet = random.choice(alphabet)
-        six_random_alphabet = random.choice(alphabet)
-        seven_random_alphabet = random.choice(alphabet)
-        eight_random_alphabet = random.choice(alphabet)
-        nine_random_alphabet = random.choice(alphabet)
-        ten_random_alphabet = random.choice(alphabet)
-#___________________|
+        global random_alphabet1, random_alphabet2, random_alphabet3, random_alphabet4, random_alphabet5, random_alphabet6, random_alphabet7, random_alphabet8, random_alphabet9
+        random_alphabet1 = random.choice(alphabet)
+        random_alphabet1 = random.choice(alphabet)
+        random_alphabet2 = random.choice(alphabet)
+        random_alphabet3 = random.choice(alphabet)
+        random_alphabet4 = random.choice(alphabet)
+        random_alphabet5 = random.choice(alphabet)
+        random_alphabet6 = random.choice(alphabet)
+        random_alphabet7 = random.choice(alphabet)
+        random_alphabet8 = random.choice(alphabet)
+        random_alphabet9 = random.choice(alphabet)
+#___________________
 def SettingVariables():
-    #setallvariables()
-    global random_number, two_random_number, three_random_number, four_random_number, five_random_number, six_random_number, seven_random_number, eight_random_number, nine_random_number, ten_random_number
-    global random_alphabet, two_random_alphabet, three_random_alphabet, four_random_alphabet, five_random_alphabet, six_random_alphabet, seven_random_alphabet, eight_random_alphabet, nine_random_alphabet, ten_random_alphabet
-    global first_combination, two_first_combination, three_first_combination, four_first_combination, five_first_combination, six_first_combination, seven_first_combination, eight_first_combination, nine_first_combination, ten_first_combination
-    first_combination = (base + "0" + "x" + random_number + random_alphabet + " " + "/")
-    two_first_combination = (base + "0" + "x" + two_random_alphabet + two_random_number + " " + "/")
-    three_first_combination = (base + "0" + "x" + three_random_number + three_random_alphabet + " " + "/")
-    four_first_combination = (base + "0" + "x" + four_random_alphabet + four_random_number + " " + "/")
-    five_first_combination = (base + "0" + "x" + five_random_number + five_random_alphabet + " " + "/")
-    six_first_combination = (base + "0" + "x" + six_random_alphabet + six_random_number + " " + "/")
-    seven_first_combination = (base + "0" + "x" + seven_random_number + seven_random_alphabet + " " + "/")
-    eight_first_combination = (base + "0" + "x" + eight_random_alphabet + eight_random_number + " " + "/")
-    nine_first_combination = (base + "0" + "x" + nine_random_number + nine_random_alphabet + " " + "/")
-    ten_first_combination = (base + "0" + "x" + ten_random_alphabet + ten_random_number + " " + "/")
-    #___________________|
+    global random_number1, random_number2, random_number3, random_number4, random_number5, random_number6, random_number7, random_number8, random_number9
+    global random_alphabet1, random_alphabet2, random_alphabet3, random_alphabet4, random_alphabet5, random_alphabet6, random_alphabet7, random_alphabet8, random_alphabet9
+    global combination1, combination2, combination3, combination4, combination5, combination6, combination7, combination8, combination9
+    combination1 = (base + "0" + "x" + random_number1 + random_alphabet1 + " " + "/")
+    combination2 = (base + "0" + "x" + random_alphabet2 + random_number1 + " " + "/")
+    combination3 = (base + "0" + "x" + random_number3 + random_alphabet3 + " " + "/")
+    combination4 = (base + "0" + "x" + random_alphabet4 + random_number4 + " " + "/")
+    combination5 = (base + "0" + "x" + random_number5 + random_alphabet5 + " " + "/")
+    combination6 = (base + "0" + "x" + random_alphabet6 + random_number6 + " " + "/")
+    combination7 = (base + "0" + "x" + random_number7 + random_alphabet7 + " " + "/")
+    combination8 = (base + "0" + "x" + random_alphabet8 + random_number8 + " " + "/")
+    combination9 = (base + "0" + "x" + random_number9 + random_alphabet9 + " " + "/")
+    #
+    combinations = [combination1]
+    #combination2, combination3, combination4, combination5, combination6, combination7, combination8, combination9
+    #___________________
 def PrintVariables():
-    #setcombinations()
-    global first_combination, two_first_combination, three_first_combination, four_first_combination, five_first_combination, six_first_combination, seven_first_combination, eight_first_combination, nine_first_combination, ten_first_combination
-    print (first_combination)
-    print (two_first_combination)
-    print (three_first_combination)
-    print (four_first_combination)
-    print (five_first_combination)
-    print (six_first_combination)
-    print (seven_first_combination)
-    print (eight_first_combination)
-    print (nine_first_combination)
-    #___________________|
+    global combination1, combination2, combination3, combination4, combination5, combination6, combination7, combination8, combination9
+    print (combination1)
+   #print (combination2)
+   #print (combination3)
+   #print (combination4)
+    #print (combination5)
+    #print (combination6)
+    #print (combination7)
+    #print (combination8)
+    #print (combination9)
+    #___________________
 def ResetVariables():
-    #setcombinations()
-    global first_combination, two_first_combination, three_first_combination, four_first_combination, five_first_combination, six_first_combination, seven_first_combination, eight_first_combination, nine_first_combination, ten_first_combination
-    del first_combination
-    del two_first_combination
-    del three_first_combination
-    del four_first_combination
-    del five_first_combination
-    del six_first_combination
-    del seven_first_combination
-    del eight_first_combination
-    del nine_first_combination
-      #___________________|
+    global combination1, combination2, combination3, combination4, combination5, combination6, combination7, combination8, combination9
+    global random_number1, random_number2, random_number3, random_number4, random_number5, random_number6, random_number7, random_number8, random_number9
+    global random_alphabet1, random_alphabet2, random_alphabet3, random_alphabet4, random_alphabet5, random_alphabet6, random_alphabet7, random_alphabet8, random_alphabet9
+    #del
+    del combination1, combination2, combination3, combination4, combination5, combination6, combination7, combination8, combination9
+    del random_number1, random_number2, random_number3, random_number4, random_number5, random_number6, random_number7, random_number8, random_number9
+    del random_alphabet1, random_alphabet2, random_alphabet3, random_alphabet4, random_alphabet5, random_alphabet6, random_alphabet7, random_alphabet8, random_alphabet9
+    #___________________
 def FinalProduct():
    GetRandomNumbers()
    GetRandomLetters()
@@ -106,15 +78,63 @@ def FinalProduct():
    PrintVariables()
    ResetVariables()
 #_________________________________________________________|
-#Create input into times to run finalproduct function
-FinalProduct()
-FinalProduct()
-FinalProduct()
-FinalProduct()
-FinalProduct()
 
-
-
-
+user_input = input("Please enter how many bytes you want to generate. (1-10):")
+#optimize this.
+if user_input == '1':
+    FinalProduct()
+if user_input == '2':
+    FinalProduct()
+    FinalProduct()
+if user_input == '3':
+    FinalProduct()
+    FinalProduct()
+    FinalProduct()
+if user_input == '4':
+    FinalProduct()
+    FinalProduct()
+    FinalProduct()
+    FinalProduct()
+if user_input == '5':
+    FinalProduct()
+    FinalProduct()
+    FinalProduct()
+    FinalProduct()
+    FinalProduct()
+if user_input == '6':
+    FinalProduct()
+    FinalProduct()
+    FinalProduct()
+    FinalProduct()
+    FinalProduct()
+    FinalProduct()
+if user_input == '7':
+    FinalProduct()
+    FinalProduct()
+    FinalProduct()
+    FinalProduct()
+    FinalProduct()
+    FinalProduct()
+    FinalProduct()
+if user_input == '8':
+    FinalProduct()
+    FinalProduct()
+    FinalProduct()
+    FinalProduct()
+    FinalProduct()
+    FinalProduct()
+    FinalProduct()
+    FinalProduct()
+if user_input == '10':
+    FinalProduct()
+    FinalProduct()
+    FinalProduct()
+    FinalProduct()
+    FinalProduct()
+    FinalProduct()
+    FinalProduct()
+    FinalProduct()
+    FinalProduct()
+    FinalProduct()
 
 
